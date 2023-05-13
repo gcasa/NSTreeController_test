2bcf2f212052ed3bf6d75f9c1c46a9bdb9f76df1
Gregory John Casamento <greg.casamento@gmail.com>
2023-05-13 17:04:32 -0400

    Reindent

--- NSTreeController_test/AppDelegate.m
+++ NSTreeController_test/AppDelegate.m
@@ -20,24 +20,35 @@
 
 - (NSArray *) buildNodes
 {
-    TreeNode *node = [[TreeNode alloc] init];
-    node.value = @"Root";
-    node.leaf = NO;
-    NSArray *array = [NSArray arrayWithObject: node];
-    TreeNode *child = [[TreeNode alloc] init];
-    child.value = @"Child 0";
-    child.leaf = YES;
-    node.children = [NSMutableArray arrayWithObject: child];
-    node.count = [node.children count];
-    
-    return array;
+  TreeNode *node = [[TreeNode alloc] init];
+  node.value = @"Root";
+  node.leaf = NO;
+  NSArray *array = [NSArray arrayWithObject: node];
+  TreeNode *child = [[TreeNode alloc] init];
+  child.value = @"Child 0";
+  child.leaf = YES;
+  node.children = [NSMutableArray arrayWithObject: child];
+  node.count = [node.children count];
+  
+  return array;
 }
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
-    // self.treeController.childrenKeyPath = @"childNodes";
-    // self.treeController.leafKeyPath = @"leaf";
-    self.nodes = [self buildNodes];
+  // Insert code here to initialize your application
+  // self.treeController.childrenKeyPath = @"childNodes";
+  // self.treeController.leafKeyPath = @"leaf";
+  
+  NSTreeNode *testNode = [[NSTreeNode alloc] init];
+  NSTreeController *testController = [[NSTreeController alloc] initWithContent: testNode];
+  NSString *countPath = [testController countKeyPathForNode:testNode];
+  NSString *childPath = [testController childrenKeyPathForNode:testNode];
+  NSString *leafPath = [testController leafKeyPathForNode:testNode];
+  
+  NSLog(@"countPath = %@", countPath);
+  NSLog(@"childPath = %@", childPath);
+  NSLog(@"leafPath = %@", leafPath);
+  
+  self.nodes = [self buildNodes];
 }
 
 
