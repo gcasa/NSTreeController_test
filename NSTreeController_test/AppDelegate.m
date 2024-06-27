44d242a53d7db00b247c5a3f57317b51622a21c6
Gregory Casamento <greg.casamento@gmail.com>
2024-06-27 05:37:56 -0400

    Update delegate

--- NSTreeController_test/AppDelegate.m
+++ NSTreeController_test/AppDelegate.m
@@ -84,7 +84,8 @@ void DumpObjcMethods(Class clz) {
     NSString *sc = NSStringFromClass([[self.treeController.arrangedObjects class] superclass]);
     NSLog(@"arrangedObjects = %@, className = %@, superClass = %@",
           self.treeController.arrangedObjects, cn, sc);
-    NSLog(@"representedObject = %@", self.treeController.arrangedObjects.representedObject);
+    NSLog(@"representedObject = %@, value = %@", self.treeController.arrangedObjects.representedObject,
+          self.treeController.arrangedObjects.representedObject.value);
     DumpObjcMethods([self.treeController.arrangedObjects class]);
 }
 
